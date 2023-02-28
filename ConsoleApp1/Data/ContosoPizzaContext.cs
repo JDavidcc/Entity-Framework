88b682fb326ea558c96e079c381d45846df9e054
David cervantes <xRaptor_XD@hotmail.com>
2023-02-27 18:33:19 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Data/ContosoPizzaContext.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp1.Data
+//{
+//    internal class ContosoPizzaContext
+//    {
+//    }
+//}
+using ConsoleApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace ConsoleApp1.Data;
+
+public class ContosoPizzaContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<OrderDetail> orderDetails { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ConsoleApp1");
+    }
+}
