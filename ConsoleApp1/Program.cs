72bca4aa01da783d4bdaba7c998aba321a736680
David cervantes <xRaptor_XD@hotmail.com>
2023-02-27 19:56:57 -0700

    Create, delete, update tables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1.Data;
+using ConsoleApp1.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+var veggieSpecial = context.Products
+    .Where(p => p.Name == "Veggie Special Pizza")
+    .FirstOrDefault();
+
+if (veggieSpecial is Product)
+{
+    veggieSpecial.Price = 10.99M;
+}
+context.SaveChanges();
+
+
+var products = from product in context.Products
+               where product.Price > 10.00M
+               orderby product.Name
+               select product;
+
+
+foreach (Product p in products)
+{
+    Console.WriteLine($"Id:     {p.Id}");
+    Console.WriteLine($"Id:     {p.Name}");
+    Console.WriteLine($"Id:     {p.Price}");
+    Console.WriteLine(new string('-', 20));
+}
